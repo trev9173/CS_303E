781326fad471b499f06495ee9350dc8ff3000929
trevorhuis <32947486+trevorhuis@users.noreply.github.com>
2018-04-01 21:25:02 -0500

    first commit

---
+++ classGrade.py
@@ -0,0 +1,27 @@
+class assignment():
+
+	def __init__(self, grade):
+		self.grade = grade
+
+	def
+
+	def inputGrade(self, grade):
+		userInput = eval(input("What grade did you recieve?")
+
+class test():
+
+	def __init__(self, grade, multiplier = .1):
+		assignment.__init__(self,grade)
+		self.multiplier = .1
+
+class codeAssign():
+
+	def __init__(self, grade, multiplier = .1):
+		assignment.__init__(self,grade)
+		self.multiplier = .1
+
+class finalCodeAssign():
+
+	def __init__(self, grade, multiplier = .1):
+		assignment.__init__(self,grade)
+		self.multiplier = .1
